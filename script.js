2e1458d9f8c2b57919ebaa16cf6f1c12ed66f3ae
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2023-12-04 18:25:02 +1100

    Added variables

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variables for the password
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "}", "|", "~", "[", "]"];
+
+// Conditions
+// Ranges between 8-128characters
+// Pop-up prompts asking to include lowercase, uppercase, numbers, specialcharacters
+// At least 1 prompt needs to be selected
+// Once answered all 4 prompts, password is generated in box or as a pop up
+
+
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
