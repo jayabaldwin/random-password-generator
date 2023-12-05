86bfe18ce8b5ddb652c47701a3ca4fb33bae9b94
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2023-12-05 16:35:28 +1100

    setting up for code, organising to visualise in chronological order

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-// Assignment Code
+// Variables for the password
+var characterLength = [];
+var userChoice = [];
+
+const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "}", "|", "~", "[", "]"];
+
+
+// 1) Assignment Code - linking to HTML button so an event listener can be added to it?
 var generateBtn = document.querySelector("#generate");
 
-// Variables for the password
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "}", "|", "~", "[", "]"];
+// 2) Add event listener to generate button - initiates the JS code from the action of being clicked?
+generateBtn.addEventListener("click", writePassword);
 
-
-function generatePassword() {
-
+// 3) Prompts for the user to choose from after generate button has been pressed
+function getPrompts() {
+  characterLength = prompt("Please enter the desired password length. It must be between 8-128 characters." );
 }
 
-// Conditions
-// Ranges between 8-128characters
-// Pop-up prompts asking to include lowercase, uppercase, numbers, specialcharacters
-// At least 1 prompt needs to be selected
-// Once answered all 4 prompts, password is generated in box or as a window.prompt
-// Write function
+// 4) Password to generate based on the prompts selected in the above function, action with the getprompts() function
+function generatePassword() {
+}
 
-
-
-
-// Write password to the #password input
+// 5) Once the passwords been generated, write password into the #password input in the HTML
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
