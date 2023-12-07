2a2a29d4c3fb213e7de08b1dc497d430a9b8ab6c
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2023-12-07 18:16:21 +1100

    clean up code

--- script.js
+++ script.js
@@ -10,28 +10,21 @@ function getPrompts() {
   var characterLength = parseInt(
     prompt('How many characters would you like your password to contain?')
   );
-  // Check to see if user length entry is a number
+  // Checks that the user entry is a valid number
   if (Number.isNaN(characterLength)) {
-    alert('Password length must be provided as a number');
+    alert('Please select a numerical value, between 8-128 characters.');
     return null;
   }
-  // Check if the password is at least 8 chars long
+  // Alert if character input is less than 8 characters
   if (characterLength < 8) {
-    alert('Password length must be at least 8 characters');
+    alert('Please select a numerical value, between 8-128 characters.');
     return null;
   }
-  // Check if the password is less than 128 characters
+  // Alert if character input exceeds 128 characters
   if (characterLength > 128) {
-    alert('Password length must less than 129 characters');
+    alert('Please select a numerical value, between 8-128 characters.');
     return null;
   }
-
-//  var characterLength = parseInt(prompt("Please enter the desired password length. It must be between 8-128 characters."), 10);
-
-// if (characterLength < 8 || characterLength > 128 || Number.isNaN(characterLength)) {
-//   alert("Please select a numerical value, between 8-128 characters.");
-//   return null;
-// } 
 
 var hasLowerCase = confirm("Click OKAY to include lowercase characters.");
 
@@ -65,9 +58,8 @@ var userChoices = {
 return userChoices;
 }
 
-console.log(getPrompts);
 
-// Function to generate random numbers, this will be used repeatedly further down
+// Function to generate random numbers, this will be used again further down
 function getRandom(arr) {
   var randIndex = Math.floor(Math.random() * arr.length);
   var randElement = arr[randIndex];
@@ -144,98 +136,3 @@ function writePassword() {
 var generateBtn = document.querySelector("#generate");
 // Event listener allows our code to respond once the generate button has been clicked, upon which the write password function will be activated
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-
-// var result = [];
-// var possibleCharacters = [];
-// var guarenteedCharacters = [];
-
-
-
-
-
-
-//   if (characterLength < 8 || characterLength > 128 || isNaN(characterLength)) {
-//     alert("Please select a numerical value, between 8-128 characters.");
-//     return null;
-//   } 
-
-// if (confirm("Click OKAY to include lowercase characters.")) {
-//   userChoices = userChoices.concat(lowerCase);
-// }
-
-// if (confirm("Click OKAY to include uppercase characters.")) {
-//   userChoices = userChoices.concat(upperCase);
-// }
-
-// if (confirm("Click OKAY to include numbers characters.")) {
-//   userChoices = userChoices.concat(numbers);
-// }
-
-// if (confirm("Click OKAY to include special characters.")) {
-//   userChoices = userChoices.concat(specialCharacters);
-// }
-// return true;
-
-
-
-// If at least one prompt is selected it should return true
-// If its true then we can set up a variable that its done correctly 
-
-// }
-
-// Function for getting a random element from an array
-
-
-
-
-
-
-// 4) Password to generate based on the prompts selected, action with the getprompts() function
-// function generatePassword() {
-//   getPrompts();
-    // 1. call getPrompts function
-
-    // array to store the result
-
-    // array to store possible characters
-
-    // array to store guarenteed characters
-
-    // check if options exisst
-
-    // add selected characters to an array of possible characters
-
-    // loop over the passwords length selecting random indicies from the possible characters
-
-    // mix in at least one of the guarenteed chars in the result
-
-    // loop over the passweords length, selecting random indicies from the possible
-
-// 2. Ensure what user inputs is valid 
-// 3. Generate password based on criteria
-
-// 4. Display generated password on page
-//   return "Generated password will go here";
-// }
-
-
-
-// 4) Once the password has been generated, writePassword function will display the value in the #password input (text-area card)
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   // dont understand what this line means... what value?
-//   passwordText.value = password;
-// }
